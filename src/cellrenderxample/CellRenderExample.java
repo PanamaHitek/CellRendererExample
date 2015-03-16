3c0af7378f63b4f003d929ec629117b393c5a4a7
Antony García González <antony.garcia.gonzalez@gmail.com>
2015-03-15 19:50:45 -0500

    Ejemplo sobre cómo cambiar los colores de las filas de un JTable

---
+++ src/cellrenderxample/CellRenderExample.java
@@ -0,0 +1,11 @@
+
+package cellrenderxample;
+
+public class CellRenderExample {
+
+    public static void main(String[] args) {
+       JFrameWindow window = new JFrameWindow();
+       window.setVisible(true);
+    }
+    
+}
